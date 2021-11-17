fcfe52b42cf7106d5bcf8f49254d7bcdf8e4c9e8
Luiz Falcão <39093175+llfalcao@users.noreply.github.com>
2021-11-17 10:33:02 -0300

    Add generic content

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 import './styles/main.css';
+
+const galleryItems = [
+  {
+    image: '',
+    text: 'this is some subtext under an illustration or image'
+  },
+  {
+    image: '',
+    text: 'this is some subtext under an illustration or image'
+  },
+  {
+    image: '',
+    text: 'this is some subtext under an illustration or image'
+  },
+  {
+    image: '',
+    text: 'this is some subtext under an illustration or image'
+  }
+];
+
+const gallery = document.querySelector('.gallery__container');
+
+galleryItems.forEach(item => `
+  <div class="gallery__item">
+    <img class="gallery__image" src=${item.image} alt="" />
+    <p class="gallery__description">${item.text}</p>
+  </div>
+`);
